627d9048e3ab854f06bde02b6d093680290ace6a
Arppit Madankar <arppitmadankar@gmail.com>
2021-07-28 19:16:19 +0530

    added container

--- src/components2/App.jsx
+++ src/components2/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import './App.css'
 
 const App = () => {
     return (
         <div>
-            <h1>Now working here</h1>
+            <nav class="navbar navbar-light " style={{paddingTop:'45px',backgroundColor:'#5353ff'}}>
+
+</nav>
+        <h1 style={{textAlign:'center',paddingTop:'20px'}}>
+            Will Creator
+        </h1>
+        <div class = 'container-fluid' style={{ width:'80%',alignSelf:'center',borderRight:'1px solid #5353ff',borderTop:'1px solid #5353ff',borderBottom:'1px solid #5353ff',borderLeft:'1px solid #5353ff', height:'100vh',borderRadius:'15px',boxShadow:'5px 5px 10px #1f1f00'}}>
+
+        
+
+        </div>
+
         </div>
     )
 }
